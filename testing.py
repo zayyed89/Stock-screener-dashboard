54269fbd62e12e0b79ccb787d2992af741d331c1
zayyed89 <zayyed03@gmail.com>
2026-05-06 19:23:55 +0530

    Update testing.py

--- testing.py
+++ testing.py
@@ -3,85 +3,74 @@ import pandas as pd
 import numpy as np
 import yfinance as yf
 import plotly.express as px
+from alpha_vantage.fundamentaldata import FundamentalData
+from stocknews import StockNews
 
-# Title of dashboard
-st.title('stock dashboard') 
+st.title('Stock Dashboard') 
 
-# Adding sidebar for Ticker , Start date and End date
 Ticker = st.sidebar.text_input('Ticker')
-start_date = st.sidebar.date_input('start date')
-end_date = st.sidebar.date_input('end date')
+start_date = st.sidebar.date_input('Start Date')
+end_date = st.sidebar.date_input('End Date')
 
 if Ticker:
     data = yf.download(Ticker, start=start_date, end=end_date)
-
     if not data.empty:
-        # Fix MultiIndex columns from yfinance
         data.columns = data.columns.get_level_values(0)
-
         fig = px.line(data, x=data.index, y='Close', title=Ticker)
         st.plotly_chart(fig)
 
-# Creating 3 new tabs 
-pricing_data, fundamental_data, news = st.tabs(['pricing data', 'fundamental data', 'news'])
+    pricing_data, fundamental_data, news = st.tabs(['Pricing Data', 'Fundamental Data', 'News'])
 
-# Mentioning the details in the pricing data tab
-with pricing_data:
-    st.header('price Movement')
-    data2 = data
-    data2['% Change'] = data['Close'].squeeze() / data['Close'].squeeze().shift(1) - 1 # Adding an extra coloumn for % change
-    data2.dropna(inplace = True) # Dropped null values
-    st.write(data2)
+    with pricing_data:
+        st.header('Price Movement')
+        data2 = data.copy()
+        data2['% Change'] = data['Close'] / data['Close'].shift(1) - 1
+        data2.dropna(inplace=True)
+        st.write(data2)
 
-# Calculating annual return
-    annual_return = data2['% Change'].mean()*252*100 # Multiplied the mean return with *252(working days) to get annual return
-    st.write('Annual return is', annual_return, '%' )
-    
-# Calculating standard deviation
-    stdev = np.std(data2['% Change'])*np.sqrt(252) 
-    st.write('standard deviation is', stdev*100, '%')
-    st.write('risk adjusted return is', annual_return/(stdev*100,))
+        annual_return = data2['% Change'].mean() * 252 * 100
+        st.write('Annual Return is', round(annual_return, 2), '%')
 
-# Importing alpha vantage api to get fundamental data of stocks
-    from alpha_vantage.fundamentaldata import FundamentalData
+        stdev = np.std(data2['% Change']) * np.sqrt(252)
+        st.write('Standard Deviation is', round(stdev * 100, 2), '%')
+
+        risk_adj_return = annual_return / (stdev * 100)
+        st.write('Risk Adjusted Return is', round(risk_adj_return, 2))
+
     with fundamental_data:
-        key = '3QGG2XI66GJ9I6ND' 
-        fd = FundamentalData(key, output_format = 'pandas')
-        st.subheader('Balance sheet')
-        balance_sheet = fd.get_balance_sheet_annual(Ticker)[0]
-        bs = balance_sheet.T[2:] # Used transpose for better data visualisation
-        bs.columns = list(balance_sheet.T.iloc[0]) 
-        st.write(bs)
+        try:
+            key = '3QGG2XI66GJ9I6ND'
+            fd = FundamentalData(key, output_format='pandas')
 
-        st.subheader('income statement')
-        income_statement = fd.get_income_statement_annual(Ticker)[0]
-        is1 = income_statement.T[2:] # Used transpose for better data visualisation
-        is1.columns = list(income_statement.T.iloc[0])
-        st.write(is1)
+            st.subheader('Balance Sheet')
+            balance_sheet = fd.get_balance_sheet_annual(Ticker)[0]
+            bs = balance_sheet.T[2:]
+            bs.columns = list(balance_sheet.T.iloc[0])
+            st.write(bs)
 
-        st.subheader('Cash Flow Statement')
-        cash_flow = fd.get_cash_flow_annual(Ticker)[0]
-        cf = cash_flow.T[2:] # Used transpose for better data visualisation
-        cf.columns = list(cash_flow.T.iloc[0])
-        st.write(cf) 
+            st.subheader('Income Statement')
+            income_statement = fd.get_income_statement_annual(Ticker)[0]
+            is1 = income_statement.T[2:]
+            is1.columns = list(income_statement.T.iloc[0])
+            st.write(is1)
 
-# Installed stocknews liabrary and using the API to get news of the stock
-from stocknews import StockNews
-with news:
-    st.header(f'News about {Ticker}')
-    sn = StockNews(Ticker, save_news = False) # We dont want to save the news (used false)
-    df_news = sn.read_rss() # Creates a dataframe of recent news
-    for i in range(10): # We need top 10 News
-        st.subheader(f'News {i+1}')
-        st.write(df_news['published'][i]) # Publised Date
-        st.write(df_news['title'][i]) # Title of news
-        st.write(df_news['summary'][i]) # Summary of news
-        title_sentiment = df_news['sentiment_title'][i] # Sentiment of title
-        st.write(f'Title Sentiment {title_sentiment}')
-        news_sentiment = df_news['sentiment_summary'][i] # Sentiment of summary
-        st.write(f'News Sentiment {news_sentiment}')   
+            st.subheader('Cash Flow Statement')
+            cash_flow = fd.get_cash_flow_annual(Ticker)[0]
+            cf = cash_flow.T[2:]
+            cf.columns = list(cash_flow.T.iloc[0])
+            st.write(cf)
 
-        
+        except ValueError as e:
+            st.warning('Alpha Vantage API rate limit reached (25 requests/day). Please try again tomorrow or upgrade your plan.')
 
-
-
+    with news:
+        st.header(f'News about {Ticker}')
+        sn = StockNews(Ticker, save_news=False)
+        df_news = sn.read_rss()
+        for i in range(10):
+            st.subheader(f'News {i+1}')
+            st.write(df_news['published'][i])
+            st.write(df_news['title'][i])
+            st.write(df_news['summary'][i])
+            st.write(f'Title Sentiment: {df_news["sentiment_title"][i]}')
+            st.write(f'News Sentiment: {df_news["sentiment_summary"][i]}')
